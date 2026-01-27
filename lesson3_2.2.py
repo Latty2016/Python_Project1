bdd7161d5e1a874aed4440d4d814ef49b9b1cabf
latifAltay <latif0706@gmail.com>
2026-01-27 15:27:50 -0500

    second file

---
+++ lesson3_2.2.py
@@ -0,0 +1,6 @@
+# input commond
+firstName = input("Enter your first name \n")
+print(firstName)
+#lastName = input("Enter your last name \n")
+#print(lastName)
+#print("Your full name is " + firstName +" "+lastName)
